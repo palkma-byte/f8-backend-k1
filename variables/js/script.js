26c28cafd4ec0ace55161e024296805aad7d5151
Pham Hoang <phamhoangbhaz@gmail.com>
2023-06-22 12:04:11 +0700

    Pham Nguyen Hoang

---
+++ variables/js/script.js
@@ -0,0 +1,12 @@
+
+
+var customerName = "Hoang";
+
+var homeAddress = "Nam Tu Liem", homeStyle, marketPrice;
+var productTitle, productDetail, productPrice;
+
+console.log(customerName);
+console.log(homeAddress);
+console.log(productDetail);
+// Khai bao bien
+// Chuyen qua tab console
---
+++ variables/note.md
@@ -0,0 +1,21 @@
+# Quy tắc đặt tên biến 
+## camelCase
+
+- customerCase
+- customerShippingAddress
+
+-> Biến, Hàm
+
+## PascalCase
+
+HomeController
+ProductModel
+
+-> Class
+
+## Undercore
+
+customer_name
+customer_shipping_address
+
+
