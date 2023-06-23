cca9382b15f0dc0b0a78d061a7b641472067e6d1
Pham Hoang <phamhoangbhaz@gmail.com>
2023-06-23 11:23:28 +0700

    Bai tap buoi 1

---
+++ Bai-Ve-Nha-Buoi1/js/script.js
@@ -0,0 +1,55 @@
+//Bai 1
+
+// let a = 10, b = 20;
+// a = a + b;
+// b = a - b;
+// a = a - b;
+
+// console.log(`Gia tri cua a la ${a}, cua b la ${b}`);
+
+//Bai 2
+
+const S = 10 + 20 + 5**10/2
+
+console.log('Gia tri cua S = '+ S );
+
+//Bai 3
+
+// let a = 21, b = 62, c = 33;
+// if ( a > b && a > c) {
+//     console.log("a la so lon nhat");
+// } else if ( b > a, b > c ){
+//     console.log("b la so lon nhat");
+// }
+// else console.log("c la so lon nhat");
+
+
+//Bai 4
+
+//  a * b > 0 ? console.log("a va b cung dau") : console.log("a va b khac dau");
+
+//Bai 5
+
+let a = 42, b = 12, c = 15, temp;
+
+if (a > b){
+    temp = a
+    a = b 
+    b = temp
+}
+
+if (a > c){
+    temp = a
+    a = c 
+    c = temp
+}
+
+if (b > c){
+    temp = b
+    b = c 
+    c = temp 
+}
+
+console.log( a + "," + b + "," + c );
+
+
