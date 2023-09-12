4c974232cff8858d54124f855115d7d9ca64efde
Pham Hoang <phamhoangbhaz@gmail.com>
2023-09-13 01:08:08 +0700

    hw23

--- HW/HW23/app.js
+++ HW/HW23/app.js
@@ -6,7 +6,7 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const session = require("express-session");
 const flash = require("connect-flash");
-var md5 = require("md5") //asdadadas
+var md5 = require("md5") 
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
